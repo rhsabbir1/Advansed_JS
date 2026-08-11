d5b3e500d38050af3beae5d07a7969adbb85cc43
unknown <rhsabbir090@gmail.com>
2026-08-11 10:19:16 +0600

    Learn js recape

---
+++ fundamental.js
@@ -0,0 +1,9 @@
+// const name = "Sabbir";
+
+// let age  = 25;
+// age = 30;
+
+// let country = "Bangldesh";
+// country = "USA";
+
+// console.log("My name is " + name + ". I am " + age + " years old. I live in " + country + ".");
