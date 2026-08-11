0aab1c08279147e91dcb084900213f68f75bff06
unknown <rhsabbir090@gmail.com>
2026-08-11 12:43:00 +0600

    ver let const typeof oparator etc

--- fundamental.js
+++ fundamental.js
@@ -1,9 +1,83 @@
-// const name = "Sabbir";
+const name = "Sabbir";
 
-// let age  = 25;
-// age = 30;
+let age  = 25;
+age = 30;
 
-// let country = "Bangldesh";
-// country = "USA";
+let country = "Bangldesh";
+
 
 // console.log("My name is " + name + ". I am " + age + " years old. I live in " + country + ".");
+
+// console.log(typeof "Rakib");
+// console.log(typeof 12);
+// console.log(typeof true);
+
+var userName = "Rakib";
+var userName = "Sabbir";
+
+
+
+let anotherName = "Ritu";
+// let anotherName = "Rina"; // SyntaxError: Identifier 'anotherName' has already been declared
+// let cannot redeclare same variable with let or const in the same scope. However, var allows redeclaration.
+
+// console.log(userName);
+// console.log(anotherName);
+
+
+// Block scope with let and const:
+
+if(true) {
+    let blockScopedVariable = "I am block scoped";
+    const blockScopedConstant = "I am also block scoped";
+
+    // console.log(blockScopedVariable); // Accessible here
+    // console.log(blockScopedConstant); // Accessible here    
+}
+
+blockScopedVariable = "Trying to change constant"; 
+// console.log(blockScopedVariable); 
+
+
+//   console.log(blockScopedVariable); // Accessible not avaiable form here
+//   console.log(blockScopedConstant); // Accessible not avaiable form here   
+
+
+// let and const are different from var in terms of scope. Variables declared with let and const are block-scoped, meaning they are only accessible within the block they are defined in. In contrast, var is function-scoped or globally scoped if declared outside a function, which can lead to unexpected behavior if not managed carefully.
+
+
+
+// oprator 
+
+let x = 10;
+let y = 5;  
+
+
+const a = 10;
+const b = 5;
+
+const sum = a + b + " Sum "; // Addition
+
+// console.log("Sum:", sum); // Output: Sum: 15
+
+// console.log(typeof sum); // Output: string
+
+// console.log(x + y); // Addition
+// console.log(x - y); // Subtraction
+// console.log(x * y); // Multiplication
+// console.log(x / y); // Division
+// console.log(x % y); // Modulus    
+
+
+const table =[ "water", "food", "clothes", "shoes"];
+
+// console.log(typeof table); // Output: object
+
+
+const person = {
+    name: "Sabbir",
+    age: 25,
+    country: "Bangladesh"
+};      
+
+// console.log(typeof person); // Output: object
